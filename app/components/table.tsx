4c4776acf21c1f79b008b27c1523d3103a9ba384
Tyler <ty.ho1126@gmail.com>
2025-06-26 17:26:47 +0800

    refactor: table

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -10,108 +10,102 @@ type Props = {
 
 export default function DataTable({ data }: Props) {
   return (
-    <div className="space-y-4 max-w-screen overflow-hidden container">
-      <div className="bg-[#0386F4] py-2.5 px-4 w-fit rounded-[3px]">
-        <p className="text-white">詳細數據</p>
-      </div>
+    <TableContainer component={Paper}>
+      <Table
+        sx={{
+          display: "block",
+          minWidth: 400,
+          maxWidth: 650,
+          overflow: "auto",
+        }}
+        stickyHeader
+      >
+        <TableBody sx={{ td: { textAlign: "right" } }}>
+          <TableRow>
+            <TableCell
+              sx={{
+                position: "sticky",
+                left: 0,
+                zIndex: 1,
+                backgroundColor: "#F6F8FA",
+                border: "1px solid #DCDFE2",
+                whiteSpace: "nowrap",
+                "&:first-of-type": { textAlign: "left" },
+              }}
+            >
+              年度月份
+            </TableCell>
 
-      <TableContainer component={Paper}>
-        <Table
-          sx={{
-            display: "block",
-            minWidth: 400,
-            maxWidth: 650,
-            overflow: "auto",
-          }}
-          stickyHeader
-        >
-          <TableBody sx={{ td: { textAlign: "right" } }}>
-            <TableRow>
+            {data.map((item) => (
               <TableCell
+                key={item.date}
                 sx={{
-                  position: "sticky",
-                  left: 0,
-                  zIndex: 1,
                   backgroundColor: "#F6F8FA",
                   border: "1px solid #DCDFE2",
                   whiteSpace: "nowrap",
-                  "&:first-of-type": { textAlign: "left" },
                 }}
               >
-                年度月份
+                {item.date}
               </TableCell>
+            ))}
+          </TableRow>
+          <TableRow>
+            <TableCell
+              sx={{
+                position: "sticky",
+                left: 0,
+                zIndex: 1,
+                backgroundColor: "#fff",
+                border: "1px solid #DCDFE2",
+                whiteSpace: "nowrap",
+                "&:first-of-type": { textAlign: "left" },
+              }}
+            >
+              月營收
+            </TableCell>
 
-              {data.map((item) => (
-                <TableCell
-                  key={item.date}
-                  sx={{
-                    backgroundColor: "#F6F8FA",
-                    border: "1px solid #DCDFE2",
-                    whiteSpace: "nowrap",
-                  }}
-                >
-                  {item.date}
-                </TableCell>
-              ))}
-            </TableRow>
-            <TableRow>
+            {data.map((item) => (
               <TableCell
+                key={item.date}
                 sx={{
-                  position: "sticky",
-                  left: 0,
-                  zIndex: 1,
-                  backgroundColor: "#fff",
                   border: "1px solid #DCDFE2",
                   whiteSpace: "nowrap",
-                  "&:first-of-type": { textAlign: "left" },
                 }}
               >
-                月營收
+                {(item.revenue / 1000).toLocaleString()}
               </TableCell>
+            ))}
+          </TableRow>
+          <TableRow>
+            <TableCell
+              sx={{
+                position: "sticky",
+                left: 0,
+                zIndex: 1,
+                backgroundColor: "#F6F8FA",
+                border: "1px solid #DCDFE2",
+                whiteSpace: "nowrap",
+                "&:first-of-type": { textAlign: "left" },
+              }}
+            >
+              月營收成長率
+            </TableCell>
 
-              {data.map((item) => (
-                <TableCell
-                  key={item.date}
-                  sx={{
-                    border: "1px solid #DCDFE2",
-                    whiteSpace: "nowrap",
-                  }}
-                >
-                  {(item.revenue / 1000).toLocaleString()}
-                </TableCell>
-              ))}
-            </TableRow>
-            <TableRow>
+            {data.map((item) => (
               <TableCell
+                key={item.date}
                 sx={{
-                  position: "sticky",
-                  left: 0,
-                  zIndex: 1,
                   backgroundColor: "#F6F8FA",
                   border: "1px solid #DCDFE2",
                   whiteSpace: "nowrap",
-                  "&:first-of-type": { textAlign: "left" },
                 }}
               >
-                月營收成長率
+                {item.growthRate}
               </TableCell>
-
-              {data.map((item) => (
-                <TableCell
-                  key={item.date}
-                  sx={{
-                    backgroundColor: "#F6F8FA",
-                    border: "1px solid #DCDFE2",
-                    whiteSpace: "nowrap",
-                  }}
-                >
-                  {item.growthRate}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
+            ))}
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
