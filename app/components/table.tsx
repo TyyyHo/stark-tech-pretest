95e9dcedae624f67667500195aabfa1b52b09f7d
Tyler <ty.ho1126@gmail.com>
2025-06-26 17:17:10 +0800

    refactor: table

--- app/components/table.tsx
+++ app/components/table.tsx
@@ -10,8 +10,8 @@ type Props = {
 
 export default function DataTable({ data }: Props) {
   return (
-    <div className="space-y-4 max-w-screen overflow-hidden container py-4 px-0.5">
-      <div className="bg-[#0386F4] py-2.5 px-4 w-fit">
+    <div className="space-y-4 max-w-screen overflow-hidden container">
+      <div className="bg-[#0386F4] py-2.5 px-4 w-fit rounded-[3px]">
         <p className="text-white">詳細數據</p>
       </div>
 
@@ -35,7 +35,7 @@ export default function DataTable({ data }: Props) {
                   backgroundColor: "#F6F8FA",
                   border: "1px solid #DCDFE2",
                   whiteSpace: "nowrap",
-                  "&:first-child": { textAlign: "left" },
+                  "&:first-of-type": { textAlign: "left" },
                 }}
               >
                 年度月份
@@ -63,7 +63,7 @@ export default function DataTable({ data }: Props) {
                   backgroundColor: "#fff",
                   border: "1px solid #DCDFE2",
                   whiteSpace: "nowrap",
-                  "&:first-child": { textAlign: "left" },
+                  "&:first-of-type": { textAlign: "left" },
                 }}
               >
                 月營收
@@ -77,7 +77,7 @@ export default function DataTable({ data }: Props) {
                     whiteSpace: "nowrap",
                   }}
                 >
-                  {item.revenue}
+                  {(item.revenue / 1000).toLocaleString()}
                 </TableCell>
               ))}
             </TableRow>
@@ -90,7 +90,7 @@ export default function DataTable({ data }: Props) {
                   backgroundColor: "#F6F8FA",
                   border: "1px solid #DCDFE2",
                   whiteSpace: "nowrap",
-                  "&:first-child": { textAlign: "left" },
+                  "&:first-of-type": { textAlign: "left" },
                 }}
               >
                 月營收成長率
@@ -112,45 +112,6 @@ export default function DataTable({ data }: Props) {
           </TableBody>
         </Table>
       </TableContainer>
-
-      {/* <TableContainer component={Paper}>
-        <Table aria-label="vertical table">
-          <TableBody>
-            <TableRow>
-              <TableCell
-                component="th"
-                scope="row"
-                sx={{
-                  fontWeight: "bold",
-                  textAlign: "center",
-                  backgroundColor: "#F6F8FA",
-                  border: "1px solid #DCDFE2",
-                  marginRight: "16px",
-                }}
-              >
-                年度月份
-              </TableCell>
-              <section className="w-full max-w-[650px] overflow-auto">
-                {data.map((item) => (
-                  <TableCell
-                    key={item.date}
-                    component="th"
-                    scope="col"
-                    sx={{
-                      fontWeight: "bold",
-                      textAlign: "center",
-                      backgroundColor: "#F6F8FA",
-                      border: "1px solid #DCDFE2",
-                    }}
-                  >
-                    {item.date}
-                  </TableCell>
-                ))}
-              </section>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer> */}
     </div>
   );
 }
