5156152542fa0b2178630d0a83a3ee6e49b97846
Tyler <ty.ho1126@gmail.com>
2025-06-26 17:27:16 +0800

    feat: finish page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,114 @@
+"use client";
+
+import { useState } from "react";
+import useStockMonthRevenue from "./hook/useStockMonthRevenue";
+import useStockInfo from "./hook/useStockInfo";
+
+import { Input } from "@mui/material";
+import Chart from "./components/chart";
+import DataTable from "./components/table";
+import TimeSelect from "./components/time-select";
+
 import Image from "next/image";
+import Label from "./components/label";
+
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth() + 1;
+const day = now.getDate();
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [stockId, setStockId] = useState<number>(2330);
+  const [time, setTime] = useState<{ start_date: string; end_date: string }>({
+    start_date: `${year - 4}-${month}-${day}`,
+    end_date: `${year}-${month}-${day}`,
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+  const { data: stockInfo, error: stockInfoError } = useStockInfo(stockId);
+  const { tableData, error: stockMonthRevenueError } = useStockMonthRevenue(stockId, time);
+
+  const isDataExist = stockInfo?.data && stockInfo.data.length > 0;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const stockId = Number(formData.get("stockId"));
+    if (stockId) {
+      setStockId(stockId);
+    }
+  };
+
+  return (
+    <>
+      <section className="container max-w-none">
+        <form onSubmit={handleSubmit} className="flex items-center justify-center">
+          <Input name="stockId" placeholder="輸入台股代號查看資料" className="w-100 px-2" />
+          <button type="submit">
+            <Image src="/search.svg" alt="search" width={24} height={24} />
+          </button>
+        </form>
+      </section>
+
+      <div className="w-full space-y-4 safe-area py-2">
+        {/* Stock Name */}
+        {isDataExist ? (
+          <h1 className="container text-left space-x-2 font-semibold text-lg">
+            <span>{stockInfo.data[0].stock_name}</span>
+            <span>({stockId})</span>
+          </h1>
+        ) : (
+          <p className="container text-left space-x-2 font-semibold text-lg">
+            <span>請輸入正確台股代號查看資料</span>
+          </p>
+        )}
+
+        {/* Error handling */}
+        {(stockMonthRevenueError || stockInfoError) && (
+          <div className="container">
+            <p className="w-full text-center">Fetch failed</p>
+          </div>
+        )}
+
+        {/* Chart */}
+        {isDataExist && (
+          <section className="w-full flex flex-col items-end gap-4 container min-h-[456px]">
+            <div className="flex items-center justify-between gap-2 w-full">
+              <Label text="每月營收" />
+              <TimeSelect setTime={setTime} />
+            </div>
+
+            {tableData && (
+              <Chart
+                xAxisData={tableData.map((item) => item.date)}
+                barData={tableData.map((item) => item.revenue)}
+                lineData={tableData.map((item) => item.growthRate)}
+              />
+            )}
+          </section>
+        )}
+
+        {/* Table */}
+        {isDataExist && (
+          <div className="space-y-4 max-w-screen overflow-hidden container">
+            <Label text="詳細數據" />
+
+            {tableData && <DataTable data={tableData} />}
+          </div>
+        )}
+
+        {/* footer */}
+        <p className="text-right">
+          圖表單位：千元，數據來自
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://finmindtrade.com/"
+            className="text-blue-500 font-semibold"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            finmindtrade.com
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </p>
+      </div>
+    </>
   );
 }
